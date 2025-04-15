91fce8e0e6130a758296a8ba76971193d01ca3a2
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-04-15 20:31:27 +0900

    many-objects-umakuikanai

--- main.js
+++ main.js
@@ -1,32 +1,54 @@
+// perspective あたりの話が全然わからない
+// 多分みてる角度の話をしてるんだけど，これを上げたら，
+// もっと俯瞰しているような角度になるんだろうか
 let container;
 let cwidth = 300;
 let cheight = 500;
 let perspective = 50;
 
+
+// eye2ground と perspective の違いがわからない
+// eye2ground が角度で，perspective は (0,0,0) からの距離を表してるのかも
+// 試してみた感じ，実際そんな感じっぽい
 let ground;
 let gwidth = 50000;
 let gheight = 50000;
 let eye2ground = 30;
 
+// なぜここでマイナス1をしているのかがよくわからない
+// 普通にイコールにしても変わらんように見えるが，
+// まあ t-kihira に聞いてみればわかるんじゃないかという話
 let road;
 let rwidth = 300;
 let rheight = 3000;
-let eye2road = eye2ground - 1
+let eye2road = eye2ground - 1 
 
 let car;
 let carsize = 100;
 
-let wall;
-let wheight = 60;
-let wwidth = 60;
+let walls = [];
+let wheight = 30;
+let wwidth = 30;
 
 const render = () => {
+    // ここの処理が本当にわかってない
+    // まあ100歩譲って，地面と道はなんとなくわかってるような
+    // どうやって道が動いていくのかってところもちゃんとやっていきたい
+    // これは下の方の update の部分も入ってるんかもしれない
+    // まず場所を移動してる，コンテナの真ん中持っていって，（そこがゼロ？）
+    // で，道路の幅分戻してるってこと
+    // height に関してはよくわかってない
+    // とりあえず板を立ててそこにはりつけていってる感じだよな
     ground.style.transform = `
     translate3d(${cwidth / 2 - gwidth / 2}px, ${cheight / 2 - gheight / 2}px, 0)
     translate3d(${heroX}px, ${eye2ground}px, 0)
     rotate3d(1,0,0, 90deg)
     `;
 
+    // これはなんなんだって話で
+    // heroY % 100 ってやってるのが，タイルをどんどん敷き変えてる感じのはず
+    // まあでも基本的に ground と同じように配置してるってわけ
+    // 90°傾けて ground に貼り付けてるってこと
     road.style.transform = `
     translate3d(${cwidth / 2 - rwidth / 2}px, ${cheight / 2 - rheight / 2}px, 0)
     translate3d(${heroX}px,${eye2road}px, 0)
@@ -34,19 +56,27 @@ const render = () => {
     translate3d(0, ${heroY % 100}px, 0)
     `;
 
-    road.style.transform = `
-    translate3d(${cwidth / 2 - rwidth / 2}px, ${cheight / 2 - rheight / 2}px, 0)
-    translate3d(${heroX}px,${eye2road}px, 0)
-    rotate3d(1,0,0, 90deg)
-    translate3d(0, ${heroY % 100}px, 0)
-    `;
-
-    wall.style.transform = `
-    translate3d(${cwidth / 2 - 100}px, 130px, -1000px)
-    translate3d(${heroX}px,${eye2road+60}px, 0)
-    rotate3d(1,0,0, 180deg)
-    translate3d(0, 0,${-heroY%1200}px)
-    `;
+    // そしてここの処理だよな，まあそれをどうやって地面に貼り付けるのかって話で
+    // そして当たり前なんだけど，流れていく必要があるんだよな，どうしようか
+    // 何かを止めておく必要があると思う？いやむしろ地面と共に流れていく必要があるのか
+    // ということは，道が流れるのと同じものをここにも与えてあげたらいい話
+    for (const (x,y,wall) of walls) {
+        // うーん，なんかうまくいかないな
+        // なんで昆布が張り付いてみたいになってるんだろうかって感じがする
+        // fromY とかいう謎の引数を決定していたと思うんだが，あれはなんのためなんだろう
+        // 結局 road とかと一緒に動かしたいという話
+        // まじでなぜ動かない？床が動くなら動くはずなんだが？
+        // render に入れてるから，毎ターン更新されるはずではある
+        // 検証をみにいった感じ，translate が入ってないんだよな，なんでだ？
+        // (結局，あれもわかってない，あのー eye2road の変換の意味？)
+        // 2列目は，hero つまりなんだ？
+        wall.style.transform = `
+        translate3d(${cwidth / 2 - wwidth}px, ${cheight/2-wheight/2}px, 0px)
+        translate3d(${heroX}px, ${eye2road}px, 0)
+        rotate3d(1,0,0, 90deg)
+        translate3d(0, ${heroY}px, 90)
+        `;
+    }
 }
 
 const init = () => {
@@ -83,6 +113,7 @@ const init = () => {
     road.style.background = 'linear-gradient(0, #00f 50%, #f00 50%)'
     road.style.backgroundSize = '100px 100px'
 
+/*
     wall = document.createElement('div')
     container.appendChild(wall)
     wall.style.position = 'absolute'
@@ -92,10 +123,26 @@ const init = () => {
     //wall.style.backgroundSize = '100px 100px'
     //wall.style.backgroundColor = 'linear-gradient(#e66465, #9198e6)'
     wall.style.backgroundColor = '#550'
+*/
 
     render()
 }
 
+walls = []
+createWalls = () => {
+    for(let i = 0; i < 10; i++){
+        x = Math.random()*100
+        y = Math.random()*100
+        div = document.createElement('div')
+        div.style.position = 'absolute'
+        div.style.height = `${wheight}px`
+        div.style.width = `${wwidth}px`
+        // console.log(Math.random()*100);
+        div.style.backgroundColor = '#550'
+        walls.push([x, y, div ])
+        container.appendChild(div)
+    }
+}
 let heroX = 0;
 let heroY = 0;
 
@@ -111,7 +158,10 @@ window.onload = async () => {
         v += 0.5;
         v -= v ** 3 * k
         heroX = Math.sin(dummy * 0.05) * rwidth / 2.2
-        console.log(heroX);
+        //console.log(heroX);
+        if (dummy%1000==1) {
+            createWalls();
+        }
         render();
         await new Promise(r => setTimeout(r, 16))
     }
